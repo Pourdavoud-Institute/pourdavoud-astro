d2dc997edeb0eb06222178841213f6c7e1b3b279
Daniel Saunders <94409438+dsaunders88@users.noreply.github.com>
2025-08-25 10:56:52 -0700

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,41 @@
+// @ts-check
+import browserslist from 'browserslist';
+import { browserslistToTargets } from 'lightningcss';
+import { defineConfig } from 'astro/config';
+import preact from '@astrojs/preact';
+// import preactVite from '@preact/preset-vite';
+import sanity from '@sanity/astro';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://pourdavoud.ucla.edu',
+    integrations: [
+        sanity({
+            projectId: '3z4imst3',
+            dataset: 'production',
+            useCdn: false,
+        }),
+        preact({ compat: true }),
+    ],
+    image: {
+        domains: ['cdn.sanity.io'],
+    },
+    prefetch: {
+        prefetchAll: true,
+    },
+    vite: {
+        css: {
+            transformer: 'lightningcss',
+            lightningcss: {
+                targets: browserslistToTargets(browserslist('defaults')),
+            },
+        },
+        build: {
+            cssMinify: 'lightningcss',
+        },
+    },
+    redirects: {
+        '/media': '/media/video-library',
+        '/media/videos': '/media/video-library',
+    },
+});
